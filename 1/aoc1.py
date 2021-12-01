c3b8c93e49d029510bf75c600de6ded7d8dae9f7
Vaascoo <vasco.cardoso.correia@tecnico.ulisboa.pt>
2021-12-01 05:17:59 +0000

    Initial Commit

---
+++ 1/aoc1.py
@@ -0,0 +1,15 @@
+raw = list(int(el.strip()) for el in open("input.ttxt").readlines())
+
+count1, count2 = 0, 0
+for i in range(1, len(raw) - 2):
+    if sum(raw[i: i + 3]) > sum(raw[i - 1: i + 2]):
+        count2 += 1
+    if(raw[i] > raw[i - 1]):
+        count1 += 1
+
+for i in range(len(raw) - 2, len(raw)):
+    if(raw[i] > raw[i - 1]):
+        count1 += 1
+
+print(count1)
+print(count2)
