d7b030c38770f856636f0c8599b80795363dd692
Vaascoo <vasco.cardoso.correia@tecnico.ulisboa.pt>
2021-12-10 14:12:01 +0000

    Day 10

--- 10/aoc10.py
+++ 10/aoc10.py
@@ -1,13 +1,13 @@
 raw = list(el.strip() for el in open("/home/vaascoo/Documents/Programming/rust/AOC21/10/input.txt").readlines())
 
-openings, closings, score1, scores, score2 = set(('(', '[', '{', '<')), set((')', ']', '}', '>')), 0, [], 0
+score1, scores, score2 = 0, [], 0
 correspondence, points = dict(zip(('(', '[', '{', '<'), (')', ']', '}', '>'))), dict(zip((')', ']', '}', '>'), (3, 57, 1197, 25137)))
 points2 = dict(zip(('(', '[', '{', '<'), (1, 2, 3, 4)))
 
 for line in raw:
     stack, corrupt, score2 =  [], False, 0
     for char in line:
-        if char in openings:
+        if char in correspondence:
             stack.append(char)
         else:
             if char == correspondence.get(stack[-1]):
