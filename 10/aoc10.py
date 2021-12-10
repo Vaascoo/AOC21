86161f4b30fed53b1697a69f17e6e6e4d32ebfaa
Vaascoo <vasco.cardoso.correia@tecnico.ulisboa.pt>
2021-12-10 14:09:07 +0000

    Day 10

--- 10/aoc10.py
+++ 10/aoc10.py
@@ -1,97 +1,27 @@
 raw = list(el.strip() for el in open("/home/vaascoo/Documents/Programming/rust/AOC21/10/input.txt").readlines())
 
-# () - 3
-# [] - 57
-# {} - 1197
-# >> - 25137
+openings, closings, score1, scores, score2 = set(('(', '[', '{', '<')), set((')', ']', '}', '>')), 0, [], 0
+correspondence, points = dict(zip(('(', '[', '{', '<'), (')', ']', '}', '>'))), dict(zip((')', ']', '}', '>'), (3, 57, 1197, 25137)))
+points2 = dict(zip(('(', '[', '{', '<'), (1, 2, 3, 4)))
 
-openings = set(('(', '[', '{', '<'))
-closings = set((')', ']', '}', '>'))
-score = 0
-
-for i in range(len(raw) - 1, -1, -1):
-    line = raw[i]
-    stack = []
-    for char in line:
-        if char in openings:
-            stack.append(char)
-        elif char in closings:
-            if char == ')':
-                if stack[-1] == '(':
-                    stack.pop(-1)
-                else:
-                    score += 3
-                    del(raw[i])
-                    break
-            elif char == ']':
-                if stack[-1] == '[':
-                    stack.pop(-1)
-                else:
-                    score += 57
-                    del(raw[i])
-                    break
-            elif char == '}':
-                if stack[-1] == '{':
-                    stack.pop(-1)
-                else:
-                    score += 1197
-                    del(raw[i])
-                    break
-            elif char == '>':
-                if stack[-1] == '<':
-                    stack.pop(-1)
-                else:
-                    score += 25137
-                    del(raw[i])
-                    break
-
-print(score)
-
-# *= 5 + 
-# () - 1
-# [] - 2
-# {} - 3
-# <> - 4
-
-def get_score(char):
-    scores = {
-        '(' : 1,
-        '[' : 2,
-        '{' : 3,
-        '<' : 4
-    }
-    return scores.get(char)
-
-
-
-scores = []
 for line in raw:
-    score = 0
-    stack = []
+    stack, corrupt, score2 =  [], False, 0
     for char in line:
-        c = None
         if char in openings:
             stack.append(char)
-        elif char in closings:
-            if char == ')':
-                if stack[-1] == '(':
-                    stack.pop(-1)
-            elif char == ']':
-                if stack[-1] == '[':
-                    stack.pop(-1)
-            elif char == '}':
-                if stack[-1] == '{':
-                    stack.pop(-1)
-            elif char == '>':
-                if stack[-1] == '<':
-                    stack.pop(-1)
-    
-    while len(stack) and (char := stack.pop(-1)):
-        score *= 5
-        score += get_score(char)
-                
+        else:
+            if char == correspondence.get(stack[-1]):
+                stack.pop(-1)
+            else:
+                score1 += points.get(char)
+                corrupt = True
+                break
 
-    scores.append(score)
+    while not corrupt and len(stack) and (char := stack.pop(-1)):
+        score2 *= 5
+        score2 += points2.get(char)
+    if not corrupt:
+        scores.append(score2)
 
-scores.sort()
-print(scores[len(scores) // 2])
+print(score1)
+print(sorted(scores)[len(scores) // 2])
